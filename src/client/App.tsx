e4522d20ba91b132e948a54460077b2aef475e7a
Camille Grings Silva <38230379+camillegrings@users.noreply.github.com>
2026-09-22 14:35:30 -0400

    fixes from main

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import { HomePage } from './pages/HomePage.js';
 import { MovieDetails } from './pages/MovieDetails.js';
 import { BookingPage } from './pages/BookingPage.js'
